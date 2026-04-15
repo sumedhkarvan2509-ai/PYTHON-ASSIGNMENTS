515013c72076c8d7325be8ec1079a49b09db093e
sumedhkarvan2509-ai <sumedhkarvan2509@gmail.com>
2026-04-15 12:14:13 +0530

    Create list_eg.py

---
+++ UNIT 1 /list_eg.py
@@ -0,0 +1,14 @@
+#Lists, Dictionaries, and Tuples in Python
+
+thislist = ["Shreyash", "Pushpak", "Soham"]
+
+student = {"Name": "Aditya", "Class": 6, "Age": 12}
+
+print(student["Name"])
+
+thislist.pop(0)
+print(thislist)
+
+thislist.append("Chaitanya")
+print(thislist)
+
